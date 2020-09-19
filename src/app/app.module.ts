0caadb06d28cb096e522ee3b69cd7023057daf32
Mike Schwall <mikschwall@gmail.com>
2020-09-19 10:23:43 -0500

    first feature branch commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { InitComponent } from './init/init.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    InitComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
   ],
   imports: [
     BrowserModule
