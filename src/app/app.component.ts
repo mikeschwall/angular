a5f86011dbcce2a501c8417592444abbab37a735
Mike Schwall <mikschwall@gmail.com>
2020-11-30 10:24:01 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ThreeComponent } from './three/three.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit {
   title = 'sept12';
+
+  mydata = "before content child";
+  @ViewChild(ThreeComponent) three:ThreeComponent;
+
+  ngOnInit() {
+
+  }
+
+  ngAfterContentInit (){
+
+    this.three.mydata = "AFTER VIEW CHILD";
+
+  }
 }
