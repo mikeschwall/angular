228a0255753af09b21e744d65837d75c182c4f41
Schwall <mike.schwall@hp.com>
2021-05-11 09:47:25 -0500

    this is first commit may11

---
+++ src/app/course.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class CourseService {
+    constructor(private http:HttpClient) {
+
+    }
+
+    getData() {
+        return this.http.get("https://jsonplaceholder.typicode.com/posts");
+    }
+
+    sendData(json) {
+        const data = JSON.stringify(json);
+        return this.http.post("https://course-data-81882.firebaseio.com/may11.json",data);
+
+    }
+
+    getFire() {
+        return this.http.get("https://course-data-81882.firebaseio.com/may11.json");
+    }
+}
---
+++ src/app/three/three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'three',
+  templateUrl: './three.component.html',
+  styleUrls: ['./three.component.css']
+})
+export class ThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
