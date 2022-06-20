78810e6e881253ba929b01e1ff3fde7a40ba13f7
Mike Schwall <mikeschwall@mikes-mbp.lan>
2022-06-20 13:31:25 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CourseService } from './todo/course.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  
+  mydata:any;
+
+  constructor(private courseService:CourseService) {
+
+  }
+
+  getData(id:number) {
+    this.courseService.getPerson(id).subscribe(item => {
+      this.mydata = item
+    })
+  }
+
+  onClick(test:any) {
+    console.log(test);
+  }
 }
---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"mike",title:"developer"},
+        {name:"tom",title:"graphic artist"},
+        {name:"beth",title:"ux lead"}
+    ];
+
+    getData():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getPerson(id:number):Observable<Employee> {
+        return of(this.employees[id])
+    }
+}
