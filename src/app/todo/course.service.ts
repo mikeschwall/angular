02691d3a0286c8d918b73e5b2478fe308e6c5377
Mike Schwall <mikeschwall@mikes-mbp.lan>
2023-01-02 10:01:22 -0600

    feature commit

---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    constructor(private httpClient:HttpClient) {}
+
+    getdata() {
+        return this.httpClient.get("https://jsonplaceholder.typicode.com/posts");
+    }
+
+    senddata(json:any) {
+        const data = JSON.stringify(json);
+        return this.httpClient.post("https://course-data-81882.firebaseio.com/jan2.json",data);
+    }
+    
+    getfire() {
+        return this.httpClient.get("https://course-data-81882.firebaseio.com/jan2.json");
+    }
+}
