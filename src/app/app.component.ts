3eba69df6f9e82f44b7acf98275b5fd2f6302377
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2023-06-19 09:22:42 -0500

    feature-june19 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ThreeComponent } from './todo/three/three.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements AfterViewInit {
+  
+  @ViewChild(ThreeComponent) three!:ThreeComponent;
+  mydata = "before content child";
+
+  ngAfterViewInit(): void {
+    this.three.mydata="AFTER VIEW CHILD";
+  }
 }
