787826456adf88c59c5c3fe7ba42b1d5d8968d6a
Mike Schwall <mikeschwall@macbook-pro.lan>
2023-09-25 15:09:14 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ThreeComponent } from './todo/three/three.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'angular';
+
+  mydata="before content child"
+  @ViewChild(ThreeComponent) three!:ThreeComponent;
+
+  ngAfterViewInit(): void {
+    this.three.mydata = "AFTER VIEW CHILD";
+  }
+}
---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    constructor(private http:HttpClient) {}
+
+    getdata() {
+      return this.http.get("https://jsonplaceholder.typicode.com/todos");
+        
+    }
+
+    sendata(json:any) {
+        const data = JSON.stringify(json);
+       return this.http.post("https://course-data-81882.firebaseio.com/sept25.json",data);
+    }
+
+    getfire() {
+        return this.http.get("https://course-data-81882.firebaseio.com/sept25.json")
+    }
+}
