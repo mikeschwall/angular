1596af8b36b6182ddbd97cca5ce858f65b6f40a3
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2023-10-02 13:45:01 -0500

    feature-oct2

---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"bill",title:"developer"},
+        {name:"frank",title:"pm"},
+        {name:"john",title:"database"}
+    ];
+
+    getdata():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getperson(id:number):Observable<Employee> {
+        return of(this.employees[id]);
+    }
+}
