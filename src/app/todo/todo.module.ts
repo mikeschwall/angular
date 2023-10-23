a00b1decb692c10950cb0989e99bc688a878d9f4
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2023-10-23 16:01:38 -0500

    feature commit

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoRoutingModule } from './todo-routing.module';
+import { AlertComponent } from './alert/alert.component';
+import { InitComponent } from './init/init.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { PlaceholderDirective } from './placeholder.directive';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CoursePipe } from './course.pipe';
+
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    InitComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent,
+    PlaceholderDirective,
+    CoursePipe
+  ],
+  imports: [
+    CommonModule,
+    TodoRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports:[
+    InitComponent,
+    AlertComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
+  ]
+})
+export class TodoModule { }
