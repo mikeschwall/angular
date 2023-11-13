27ef4c15f0c8fde37839c979bfda942b08a6cc48
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2023-11-13 14:24:11 -0600

    feature commit

---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"bill",title:"developer"},
+        {name:"gary",title:"database"},
+        {name:"john",title:"graphics"}
+    ];
+
+    getdata():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getperson(id:number):Observable<Employee> {
+        return of(this.employees[id])
+    }
+}
---
+++ src/app/todo/two/two.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../course.service';
+
+@Component({
+  selector: 'two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.css']
+})
+export class TwoComponent implements OnInit {
+
+  mydata:any;
+  mydata2:any;
+
+  constructor(private route:ActivatedRoute, private courseService:CourseService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(item => this.mydata = item["id"]);
+    console.log(this.mydata);
+    this.courseService.getperson(this.mydata).subscribe(item => this.mydata2 = item)
+    console.log(this.mydata)
+  }
+
+}
