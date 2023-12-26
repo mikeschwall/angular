688791825451dd702b6561ad83a9701f52be8e3e
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2023-12-26 10:11:30 -0600

    tuesday commit

---
+++ src/app/todo/init/init.component.ts
@@ -0,0 +1,24 @@
+import { Component, ComponentFactoryResolver, OnInit, ViewChild } from '@angular/core';
+import { AlertComponent } from '../alert/alert.component';
+import { PlaceholderDirective } from '../placeholder.directive';
+
+@Component({
+  selector: 'init',
+  templateUrl: './init.component.html',
+  styleUrls: ['./init.component.css']
+})
+export class InitComponent implements OnInit {
+  @ViewChild(PlaceholderDirective)ph!:PlaceholderDirective;
+
+  constructor(private componentFactoryResolver:ComponentFactoryResolver) { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    const factory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const position = this.ph.viewContainerRef;
+    position.createComponent(factory);
+  }
+
+}
