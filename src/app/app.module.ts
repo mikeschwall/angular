16f7be0f528e2038d6da4460c8abe99ffb6a4f06
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2024-02-04 12:42:58 -0600

    feature feb4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from './todo/todo.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes:Routes = []
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"john",title:"developer"},
+        {name:"bill",title:"graphics"},
+        {name:"bob",title:"pm"}
+    ];
+
+    getdata():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getperson(id:number):Observable<Employee> {
+        return of(this.employees[id]);
+    }
+}
