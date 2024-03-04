71ab18c37c862cf652f43f5a95c08fabbfb1b5e1
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2024-03-04 16:54:14 -0600

    mar 4 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CourseService } from './todo/course.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  mydata:any;
+
+  constructor(private courseService:CourseService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  getdata(test:any) {
+    this.courseService.getperson(test).subscribe(item => this.mydata = item);
+  }
+
+  final(test:any) {
+    console.log(test)
+  }
+
 }
---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"john",title:"developer"},
+        {name:"ted",title:"graphic arts"},
+        {name:"gary",title:"database admin"}
+    ];
+
+    getdata():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getperson(id:number):Observable<Employee> {
+        return of(this.employees[id]);
+    }
+}
