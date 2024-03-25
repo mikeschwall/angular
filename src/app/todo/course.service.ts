d600886a0619a32b6f30bfb6a256c7948f07361f
Mike Schwall <mikeschwall@Mikes-MacBook-Pro.local>
2024-03-25 11:00:00 -0500

    feature-mar25

---
+++ src/app/todo/course.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
+interface Employee {
+    name:string;
+    title:string;
+}
+
+@Injectable({providedIn:'root'})
+export class CourseService {
+    employees:Employee[] = [
+        {name:"bill",title:"developer"},
+        {name:"john",title:"graphic artist"},
+        {name:"frank",title:".net developer"}
+    ];
+
+    getdata():Observable<Employee[]> {
+        return of(this.employees);
+    }
+
+    getperson(id:number):Observable<Employee> {
+        return of(this.employees[id])
+    }
+}
---
+++ src/app/todo/todo-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { InitComponent } from './init/init.component';
+
+const routes: Routes = [
+  {path:"",component:OneComponent},
+  {path:"two/:id",component:TwoComponent},
+  {path:"three",component:ThreeComponent},
+  {path:"init",component:InitComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TodoRoutingModule { }
